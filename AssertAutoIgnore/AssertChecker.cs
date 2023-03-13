9f2d4589e4ae664b508e33c3256558d41dff057a
poheniks <66043010+poheniks@users.noreply.github.com>
2023-03-13 01:15:33 -0500

    Rewrote as a standalone executable that can catch all asserts, even before modules are loaded

---
+++ AssertAutoIgnore/AssertChecker.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace AssertAutoIgnore
+{
+    public class AssertChecker
+    {
+        public int? BannerlordProcessID;
+
+        public virtual void OnTick()
+        {
+            if (BannerlordProcessID.HasValue)
+            {
+                Process bannerlordProcess;
+                try
+                {
+                    Process.GetProcessById(BannerlordProcessID.Value).Refresh();
+                    bannerlordProcess = Process.GetProcessById(BannerlordProcessID.Value);
+                }
+                catch
+                {
+                    Console.WriteLine("Bannerlord process lost!");
+                    BannerlordProcessID = null;
+                    return;
+                }
+
+
+                if (bannerlordProcess == null || (!bannerlordProcess.ProcessName.Contains("Bannerlord") & !bannerlordProcess.ProcessName.Contains("MountAndBlade")))
+                {
+                    Console.WriteLine("Bannerlord process lost!");
+                    BannerlordProcessID = GetBannerlordProcess();
+                    return;
+                }
+
+                CheckForDialogBoxes(bannerlordProcess);
+            }
+            else BannerlordProcessID = GetBannerlordProcess();
+        }
+
+        private void CheckForDialogBoxes(Process process)
+        {
+            if (!process.MainWindowTitle.Contains("Bannerlord"))
+            {
+                switch (process.MainWindowTitle)
+                {
+                    case "RGL CONTENT WARNING":
+                    case "RGL CONTENT ERROR":
+                        FindAndInvokeButtonFromProcess(process, "OK");
+                        break;
+                    case "Safe Mode":
+                    case "*_*":
+                        FindAndInvokeButtonFromProcess(process, "No");
+                        break;
+                    case "Always Ignore?":
+                        FindAndInvokeButtonFromProcess(process, "OK");
+                        break;
+                    case "ASSERT":
+                        FindAndInvokeButtonFromProcess(process, "Ignore");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private int? GetBannerlordProcess()
+        {
+            Process[] processes = Process.GetProcesses();
+            Process bannerlordProcess = processes.Where(process => process.ProcessName.Contains("Bannerlord") || process.ProcessName.Contains("MountAndBlade")).FirstOrDefault();
+            if (bannerlordProcess != null)
+            {
+                Console.WriteLine("Found Bannerlord process!");
+                int bannerlordProcessID = bannerlordProcess.Id;
+
+                return bannerlordProcessID;
+            }
+
+            return null;
+        }
+
+        private void FindAndInvokeButtonFromProcess(Process process, string buttonName)
+        {
+            Console.WriteLine($"Closing: {process.MainWindowTitle}");
+
+            AutomationElement bannerlordRoot = AutomationElement.FromHandle(process.MainWindowHandle);
+
+            PropertyCondition findAssert = new PropertyCondition(AutomationElement.NameProperty, buttonName);
+            AutomationElement buttonElement = bannerlordRoot.FindFirst(TreeScope.Descendants, findAssert);
+
+            InvokePattern pattern = buttonElement.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            pattern.Invoke();
+        }
+    }
+}
---
+++ AssertAutoIgnore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Automation;
+
+namespace AssertAutoIgnore
+{
+    internal static class Program
+    {
+        private const string name = "Bannerlord Assert Auto Ignore - by poheniks";
+
+        [STAThread]
+        static void Main()
+        {
+            Console.Title = name;
+
+            Console.WriteLine("Starting Assert Auto Ignore");
+            Console.WriteLine("Looking for Bannerlord Process...");
+
+            AssertChecker assertChecker = new AssertChecker();
+            while (true)
+            {
+                assertChecker.OnTick();
+            }
+        }
+    }
+
+
+}
